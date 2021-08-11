b85925f58e4d41ae3e430f56d6445166e1cc2678
WooLyung <vlfkdydtk@naver.com>
2021-08-11 20:54:33 +0900

    수업 자료 추가

--- 과제/3일차/소스/main.cpp
+++ 과제/3일차/소스/main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
-#pragma warning disable(4996)
+#pragma warning(disable: 4996)
 using namespace std;
 
 int main()
 {
-	int n, m;
-	int a = 0;
-	int c = 0;
-	cin >> n >> m;
-	for (int i = n; i <= m; i++)
-	{
-		int b = 0;
-		for (int j = 1; j <= i; j++)
-			if (!(i % j)) b++;
-		if (b == 2)
-		{
-			a += i;
-			if (!c) c = i;
-		}
-	}
-	if (a)
-		cout << a << endl << c;
-	else
-		cout << -1;
+	int a, b, c;
+	cin >> a >> b >> c;
+
 }
